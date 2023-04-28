7b888ed6ebf22b90a27424e22a8ea54a7be43181
DanushAthithya <83062926+DanushAthithya@users.noreply.github.com>
2023-04-28 16:03:36 +0530

    add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { BsFillTelephoneFill } from "react-icons/bs";
 import { useState } from "react";
 import PhoneVer from './phonever';
 import logo from './logo.png'
+import { FaPhoneAlt } from 'react-icons/fa';
+import GoogleFonts from 'google-fonts';
 
 function App() {
 
@@ -41,32 +43,33 @@ createUserWithEmailAndPassword(auth, email, password)
   else
   {
   return (
-    <div className="App bg-gradient-to-r from-zinc-950 via-indigo-800 to-zinc-950s">
-        <div className="bg-gradient-to-r from-zinc-950 via-indigo-800 to-zinc-950 Log_box">
-          <div id="logo">
+    <div className="App bg-white">
+        <div className="bg-gradient-to-r from-zinc-950 via-indigo-800 to-zinc-950 pb-4 Log_box mx-auto d-block">
+          <br/>
+          <br/>
+          <div id="logo" className="mx-auto d-block">
             <img src={logo} />
           </div>
+          <br />
+          <p className="hd">Sign In</p>
           <div id="head">
-            <h1>Sign In</h1>
-            <p>Sign In To Continue</p>
           </div>
-          <input type="text" name="username" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}/>
+          <input className="in1 mb-5 mt-5 text-black rounded-lg p-2" type="text" name="username" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}/>
           <br/>
-          <input type="password" name="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}}/>
+          <input className="in2 mb-5 text-black rounded-lg p-2" type="password" name="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}}/>
           <br/>
-          <button onClick={SignIn}>Sign In</button>
+          <button className="bg-orange-400 p-2 pl-8 pr-8 text-black rounded-lg m-3 sig" onClick={SignIn}>Sign In</button>
           <br/>
           <br/>
           <button class="login-with-google-btn" onClick={signInWithGoogle}>
             Sign in with Google
           </button>
-          <h1>{localStorage.getItem("name")}</h1>
-          <h1>{localStorage.getItem("email")}</h1>
-          <img src={localStorage.getItem("profilePic")} />
           <br/>
-          <br/>
-          <br/>
-          <a  class="button_Phn" href="/Phone" >Sign In with Phone</a>
+          <br />
+          <a className="button_Phn btn-success bg-white-900 rounded mb-8" href="/Phone">
+            <FaPhoneAlt className="text-green-300" />
+            Sign In with Phone
+          </a>
         </div>
     </div>
   );
